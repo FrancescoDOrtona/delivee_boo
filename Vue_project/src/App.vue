cfe446d53ac35ac8e52e54410663e52f67ecbedf
FrancescoDOrtona <francescod.97@hotmail.it>
2024-02-02 16:38:50 +0100

    Add: Vue components + Variables

--- Vue_project/src/App.vue
+++ Vue_project/src/App.vue
@@ -1,30 +1,32 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
+
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter
+  },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <AppHeader />
+  <div class="flex-grow-1">
+    <router-view></router-view>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppFooter />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style lang="scss">
+@use "./style/general.scss";
+
+#app{
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
+
 </style>
---
+++ Vue_project/src/components/AppHeader.vue
@@ -0,0 +1,44 @@
+<template>
+    <header>
+        <div class="header-logo">
+            <img src="../../public/logo-teal.svg" alt="">
+        </div>
+        <div class="header-buttons">
+            <div class="dropdown">
+                <button class="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="fa-solid fa-link"></i> Links
+                </button>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </div>
+        </div>
+    </header>
+    
+    
+</template>
+
+<script>
+    export default {
+        
+    }
+
+</script>
+
+<style lang="scss" scoped>
+header{
+    background-color: black;
+    display: flex;
+    justify-content: space-between;
+    padding: 20px;
+}
+
+.header-logo{
+    margin-left: 50px;
+    img{
+        width: 150px;
+    }
+}
+</style>
