fc3edbce31604dbc372095d9c7ac415759e0f767
Ste79ITA <nastri.s@gmail.com>
2024-02-02 16:58:17 +0100

    Added homepage tab title

--- Vue_project/src/components/pages/Homepage.vue
+++ Vue_project/src/components/pages/Homepage.vue
@@ -1,11 +1,14 @@
 <script>
-
+export default {
+  name: 'Home',
+  mounted() {
+    document.title = 'DeliveBoo - Very Tasty Takeaway Food for Devs';
+  },
+};
 </script>
 
 <template>
   <h1>Delivebo</h1>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
