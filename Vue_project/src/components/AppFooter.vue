4a9f67cb0633152e9e089cd88ad6913c8549aa14
FrancescoDOrtona <francescod.97@hotmail.it>
2024-02-04 17:36:37 +0100

    Add: footer base scheme

--- Vue_project/src/components/AppFooter.vue
+++ Vue_project/src/components/AppFooter.vue
@@ -1,7 +1,14 @@
 <template>
-    <div>
-        <h1>footer</h1>
-    </div>
+    <footer>
+        <div class="container">
+            <div class="grid">
+                <div class="col-3">1</div>
+                <div class="col-3">2</div>
+                <div class="col-3">3</div>
+                <div class="col-3">4</div>
+            </div>
+        </div>
+    </footer>
 </template>
 
 <script>
@@ -11,5 +18,16 @@
 </script>
 
 <style lang="scss" scoped>
+@import "../../style/partials/variables";
 
+footer{
+    padding: 20px;
+    color: white;
+    background-color: $dark-gray-footer;
+}
+
+.grid{
+    display: grid;
+    grid-template-columns: repeat(4 , 1fr)
+}
 </style>
