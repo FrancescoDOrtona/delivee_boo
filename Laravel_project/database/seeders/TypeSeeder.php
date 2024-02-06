cddd72368c06d5fe9048b82254bd15bcaf002b5d
FrancescoDOrtona <francescod.97@hotmail.it>
2024-02-06 17:19:40 +0100

    Add: types img migration, seeder, Vue homepage types v-for loop

--- Laravel_project/database/seeders/TypeSeeder.php
+++ Laravel_project/database/seeders/TypeSeeder.php
@@ -13,11 +13,27 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        $types = ['pizzeria','sushi','kebab','cucina indiana','fast food','pub','piadineria','pasticceria','gelateria','rosticceria','ristorante','yogurteria'];
+        $types = ['pizza','sushi','kebab','indiano','fast food','pub','piadina','pasticceria','gelato','rosticceria','ristorante','yogurteria'];
 
-        foreach($types as $type){
+        $img_urls = [
+            'https://www.buttalapasta.it/wp-content/uploads/2017/11/pizza-margherita.jpg',
+            'https://img2.storyblok.com/filters:format(webp)/f/62776/512x256/8bd9a1cb99/bowl-wide.jpg',
+            'https://www.vicenzatoday.it/~media/horizontal-hi/3310647164206/kebab1-2-3.jpg',
+            'https://media-assets.lacucinaitaliana.it/photos/61faa3986a4f2380a67589eb/1:1/w_1280%2Cc_limit/13da6d68-315b-4b78-a520-af4091b2d23e.jpg',
+            'https://media-assets.lacucinaitaliana.it/photos/61fa51881c69086afcf291b8/3:2/w_1155,h_770,c_limit/mangiare-al-fastfood.jpg',
+            'https://birrasanbiagio.com/wp-content/uploads/2019/01/birra-artigianale-per-pub-rubinetti.jpg',
+            'https://www.cucchiaio.it/content/cucchiaio/it/ricette/2009/12/ricetta-piadina-romagnola/_jcr_content/header-par/image_single.img.jpg/1544440397321.jpg',
+            'https://www.salepepe.it/files/2018/03/Pasticcerie-Milano.jpg',
+            'https://www.focus.it/images/2020/09/03/gelato_1020x680.jpg',
+            'https://www.soniaperonaci.it/wp-content/uploads/2019/03/Rosticceria-siciliana-2.jpg',
+            'https://langhe.net/wp-content/uploads/2019/02/pacchetto-mangiare4.jpg',
+            'https://media-cdn.tripadvisor.com/media/photo-s/07/54/7d/f8/bel-bel-yogurteria.jpg'
+        ];
+
+        foreach($types as $index => $type){
             $new_type = new Type();
             $new_type->name = $type;
+            $new_type->img_url = $img_urls[$index];
             $new_type->save();
 
         }
--- Vue_project/src/components/pages/homepage/HomepageTypes.vue
+++ Vue_project/src/components/pages/homepage/HomepageTypes.vue
@@ -5,88 +5,24 @@
             <p>Seleziona la categoria che preferisci e scegli cosa prenotare tra una moltitudine di aziende partner.</p>
         </div>
         <div class="types-section_badges top-row">
-            <div class="badge">
-                <img  class="badge-img" src="https://img2.storyblok.com/filters:format(webp)/f/62776/512x256/8bd9a1cb99/bowl-wide.jpg" alt="">
+            <div v-for="type in store.dataRT.types" :key="type" class="badge">
+                <img  class="badge-img" :src="type.img_url" alt="">
                 <div class="badge-title">
-                    <h5>Sushi</h5>
+                    <h5>{{ type.name }}</h5>
                 </div>
             </div>
-            <div class="badge">
-                <img  class="badge-img" src="https://www.buttalapasta.it/wp-content/uploads/2017/11/pizza-margherita.jpg" alt="">
-                <div class="badge-title">
-                    <h5>Pizza</h5>
-                </div>
-            </div>
-            <div class="badge">
-                <img  class="badge-img" src="https://www.vicenzatoday.it/~media/horizontal-hi/3310647164206/kebab1-2-3.jpg" alt="">
-                <div class="badge-title">
-                    <h5>Kebab</h5>
-                </div>
-            </div>
-            <div class="badge">
-                <img  class="badge-img" src="https://media-assets.lacucinaitaliana.it/photos/61faa3986a4f2380a67589eb/1:1/w_1280%2Cc_limit/13da6d68-315b-4b78-a520-af4091b2d23e.jpg" alt="">
-                <div class="badge-title">
-                    <h5>Indiano</h5>
-                </div>
-            </div>
-            <div class="badge">
-                <img  class="badge-img" src="https://media-assets.lacucinaitaliana.it/photos/61fa51881c69086afcf291b8/3:2/w_1155,h_770,c_limit/mangiare-al-fastfood.jpg" alt="">
-                <div class="badge-title">
-                    <h5>Fast Food</h5>
-                </div>
-            </div>
-            <div class="badge">
-                <img  class="badge-img" src="https://birrasanbiagio.com/wp-content/uploads/2019/01/birra-artigianale-per-pub-rubinetti.jpg" alt="">
-                <div class="badge-title">
-                    <h5>Pub</h5>
-                </div>
-            </div>
-        </div>
-        <div class="types-section_badges bottom-row">
-            <div class="badge">
-                <img  class="badge-img" src="https://www.cucchiaio.it/content/cucchiaio/it/ricette/2009/12/ricetta-piadina-romagnola/_jcr_content/header-par/image_single.img.jpg/1544440397321.jpg" alt="">
-                <div class="badge-title">
-                    <h5>Piadina</h5>
-                </div>
-            </div>
-            <div class="badge">
-                <img  class="badge-img" src="https://www.salepepe.it/files/2018/03/Pasticcerie-Milano.jpg" alt="">
-                <div class="badge-title">
-                    <h5>Pasticceria</h5>
-                </div>
-            </div>
-            <div class="badge">
-                <img  class="badge-img" src="https://www.focus.it/images/2020/09/03/gelato_1020x680.jpg" alt="">
-                <div class="badge-title">
-                    <h5>Gelato</h5>
-                </div>
-            </div>
-            <div class="badge">
-                <img  class="badge-img" src="https://www.soniaperonaci.it/wp-content/uploads/2019/03/Rosticceria-siciliana-2.jpg" alt="">
-                <div class="badge-title">
-                    <h5>Rosticceria</h5>
-                </div>
-            </div>
-            <div class="badge">
-                <img  class="badge-img" src="https://langhe.net/wp-content/uploads/2019/02/pacchetto-mangiare4.jpg" alt="">
-                <div class="badge-title">
-                    <h5>Ristorante</h5>
-                </div>
-            </div>
-            <div class="badge">
-                <img  class="badge-img" src="https://media-cdn.tripadvisor.com/media/photo-s/07/54/7d/f8/bel-bel-yogurteria.jpg" alt="">
-                <div class="badge-title">
-                    <h5>Yogurteria</h5>
-                </div>
-            </div>
-            
-        </div>
+        </div>           
     </div>
 </template>
 
 <script>
+import { store } from '../../../store';
     export default {
-        
+        data() {
+            return {
+                store,
+            }
+        },
     }
 </script>
 
@@ -154,10 +90,10 @@
     }
 }
 
-.top-row .badge:nth-child(odd){
+.badge:nth-child(odd):nth-child(-n+5){
     grid-column: span 2;
 }
-.bottom-row .badge:nth-child(even){
+.badge:nth-child(even):nth-child(n+7){
     grid-column: span 2;
 }
 
