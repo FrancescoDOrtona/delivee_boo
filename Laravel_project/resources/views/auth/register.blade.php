4e091597437c53b0954ee6e01652836fc2d17958
Ste79ITA <nastri.s@gmail.com>
2024-02-08 10:59:28 +0100

    ADDED mail validation and comment

--- Laravel_project/resources/views/auth/register.blade.php
+++ Laravel_project/resources/views/auth/register.blade.php
@@ -29,13 +29,16 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" oninput="mailValidation()">
 
                                 @error('email')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
                                 @enderror
+                            </div>
+                            <div class="mb-2 mt-1 row">
+                                <div id="emailConfirmMessage" class="confirm-message col-md-6"></div>
                             </div>
                         </div>
 
@@ -61,9 +64,8 @@
                             </div>
                             
                         </div>
-                        <div class="mb-4 row">
-
-                            <div id="confirmMessage" class="confirm-message col-md-6"></div>
+                        <div class="mb-1 row">
+                             <div id="confirmMessage" class="confirm-message col-md-6"></div>
                         </div>
                         <div class="mb-4 row mb-0">
                             <div class="col-md-6 offset-md-4">
@@ -83,21 +85,53 @@
 
 
 <script> 
+// Password and confirm password validation
 function ConfirmPasswordMatch() {
+    // get the password and confirm password values
     let password = document.getElementById('password').value;
     let confirmPassword = document.getElementById('password-confirm').value;
     
+    // if values doesn't match
     if (password !== confirmPassword) {
+        // display an error message
         document.getElementById('confirmMessage').innerHTML = 'Password do not match!';
+        // add a class to style the error
         document.getElementById('password-confirm').classList.add('confirm-error');
+        // disable submit button
         document.getElementById("submit").disabled = true;
         
     } else {
+        // no error message
         document.getElementById('confirmMessage').innerHTML = '';
+        // error class removed
         document.getElementById('password-confirm').classList.remove('confirm-error');
+        // submit button is now working
         document.getElementById("submit").disabled = false;
     }
 }
 
+// Email validation
+function mailValidation() {
+// get the email input value
+const emailInput = document.getElementById('email').value;
+// validate email format using regular expression
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(emailInput)) {
+    // Display error message
+    document.getElementById('emailConfirmMessage').innerHTML = 'Please enter a valid email address!';
+    // add a class to style the error
+    document.getElementById('email').classList.add('confirm-error');
+    // disable submit button
+    document.getElementById("submit").disabled = true;
+
+    } else {
+        // no error message
+        document.getElementById('emailConfirmMessage').innerHTML = '';
+        // error class removed
+        document.getElementById('email').classList.remove('confirm-error');
+        // submit button is now working
+        document.getElementById("submit").disabled = false;
+    }
+}
 </script>
 
