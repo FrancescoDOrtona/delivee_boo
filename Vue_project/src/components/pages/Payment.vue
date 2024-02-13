2f2ffe884eb2d25f8529d1ec59eb1d174a4c70e9
Ste79ITA <nastri.s@gmail.com>
2024-02-13 18:01:08 +0100

    ADDED braintree vue

---
+++ Vue_project/src/components/pages/Payment.vue
@@ -0,0 +1,26 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {};
+  },
+  components: {},
+};
+</script>
+
+<template>
+  <h1>Pagina pagamento</h1>
+  <div>
+    <v-braintree
+      :authorization="xxxxxxxxxxxxxxxxx"
+      locale="it_IT"
+      @success="onSuccess"
+      @error="onError"
+    >
+    </v-braintree>
+  </div>
+  <div id="dropin-container"></div>
+</template>
+
+<style lang="scss" scoped></style>
