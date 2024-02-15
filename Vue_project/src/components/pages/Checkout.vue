58fdc893de934928fb26e4b29193e2070d48cc59
FrancescoDOrtona <francescod.97@hotmail.it>
2024-02-15 22:17:49 +0100

    Add: style to checkout page

--- Vue_project/src/components/pages/Checkout.vue
+++ Vue_project/src/components/pages/Checkout.vue
@@ -1,87 +1,93 @@
 <template>
-    <div class="container w-150">
-        <div class="row">
-            <div class="col-12 col-md-6">
-                <form @submit.prevent="submitForm" class="row g-3">
-                    <div class="col-md-6">
-                        <label for="name" class="form-label">Nome</label>
-                        <input type="text" v-model="customer.name" class="form-control" id="name">
-                    </div>
-                    <div class="col-md-6">
-                        <label for="last_name" class="form-label">Cognome</label>
-                        <input type="text" v-model="customer.last_name" class="form-control" id="last_name">
-                    </div>
-                    <div class="col-12">
-                        <label for="email" class="form-label">Email</label>
-                        <input type="email" v-model="customer.email" class="form-control" id="email">
-                    </div>
-                    <div class="col-12">
-                        <label for="address" class="form-label">Indirizzo</label>
-                        <input type="text" v-model="customer.address" class="form-control" id="address"
-                            placeholder="via devi paga!">
-                    </div>
-                    <div class="col-12">
-                        <label for="phone_number" class="form-label">Numero di telefono</label>
-                        <input type="text" v-model="customer.phone_number" class="form-control" id="phone_number"
-                            placeholder="333333333333">
-                    </div>
-
-
-                    <div class="col-12">
-                        <!-- Button trigger modal -->
-                        <button @click="sendOrder()" type="submit" class="btn btn-primary" data-bs-toggle="modal"
-                            data-bs-target="#exampleModal">
-                            Paga!
-                        </button>
-                    </div>
-                </form>
-
-            </div>
-            <div class="col-12 col-md-6">
-                <ul v-for="c in cart">
-                    <li>{{ c.name }}</li>
-                    <li>{{ c.price }}</li>
-                    <li>{{ c.quantity }}</li>
-                </ul>
-            </div>
-        </div>
-
-        {{ this.currentOrderId }}
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"
-            ref="exampleModal">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">Completa l'acquisto e termina il tuo ordine.
-                        </h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <div></div>
-                        <div id="dropin-container"></div>
-                        <!-- Messaggio di successo o di errore -->
-                        <div v-if="paymentStatus !== null" class="alert"
-                            :class="{ 'alert-success': paymentStatus, 'alert-danger': !paymentStatus }">
-                            {{ paymentStatus ? 'Pagamento completato con successo!' : 'Errore durante il pagamento.' }}
+    <main>
+        <div class="bg-img"></div>
+        <div class="container w-150">
+            <div class="row flex-md-nowrap gap-2">
+                <div class="col-12 col-md-6 customer-form">
+                    <h2 class="border_btm pb-2">Dati di spedizione</h2>
+                    <form @submit.prevent="submitForm" class="row g-3">
+                        <div class="col-md-6">
+                            <label for="name" class="form-label fw-bold">Nome</label>
+                            <input type="text" v-model="customer.name" class="form-control" id="name" placeholder="Mario">
                         </div>
+                        <div class="col-md-6">
+                            <label for="last_name" class="form-label fw-bold">Cognome</label>
+                            <input type="text" v-model="customer.last_name" class="form-control" id="last_name" placeholder="Rossi">
+                        </div>
+                        <div class="col-12">
+                            <label for="email" class="form-label fw-bold">Email</label>
+                            <input type="email" v-model="customer.email" class="form-control" id="email" placeholder="mario.rossi@deliveboo.it">
+                        </div>
+                        <div class="col-12">
+                            <label for="address" class="form-label fw-bold">Indirizzo</label>
+                            <input type="text" v-model="customer.address" class="form-control" id="address"
+                                placeholder="Viale Cappuccini 87">
+                        </div>
+                        <div class="col-12">
+                            <label for="phone_number" class="form-label fw-bold">Numero di telefono</label>
+                            <input type="text" v-model="customer.phone_number" class="form-control" id="phone_number"
+                                placeholder="320 6745938">
+                        </div>
+
+
+                        <div class="col-12">
+                            <!-- Button trigger modal -->
+                            <button @click="sendOrder()" type="submit" class="btn btn-light btn-form fw-bold" data-bs-toggle="modal"
+                                data-bs-target="#exampleModal">
+                                Procedi al pagamento
+                            </button>
+                        </div>
+                    </form>
+
+                </div>
+                <div class="col-12 col-md-6 summary">
+                    <h2 class="border_btm pb-2">Riepilogo Ordine</h2>
+                    <div class="summary_list_container">
+                        <ul class="border_btm summary_list" v-for="c in cart">
+                            <li>{{ c.name }}</li>
+                            <li>{{ c.price }} €</li>
+                            <li>x{{ c.quantity }}</li>
+                        </ul>
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
-                        <!-- Rimuovi l'attributo data-bs-dismiss dal pulsante Paga -->
-                        <button id="submit-button" class="button button--small button--green">
-                            Paga
-                        </button>
+                    <h4 class="border_top pt-2">Totale: {{ this.currentTotal }}</h4>
+                </div>
+            </div>
+
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"
+                ref="exampleModal">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Completa l'acquisto e termina il tuo ordine.
+                            </h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <div></div>
+                            <div id="dropin-container"></div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="button button--small button--gray"
+                                data-bs-dismiss="modal">Chiudi</button>
+                            <!-- Rimuovi l'attributo data-bs-dismiss dal pulsante Paga -->
+                            <button id="submit-button" class="button button--small button--green">
+                                Paga
+                            </button>
+                        </div>
+                        <!-- Loader per il pagamento -->
+                        <div v-if="isLoading" class="loading">
+                            <span class="loader"></span>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </main>
 </template>
 
 <script>
 import axios from 'axios';
-import $ from 'jquery';
 export default {
 
     data() {
@@ -97,8 +103,9 @@ export default {
             },
             cart: [],
             currentOrderId: 0,
+            currentTotal: 0,
             token: '',
-            paymentStatus: null
+            isLoading: false
         }
     },
     methods: {
@@ -162,14 +169,22 @@ export default {
                                     .then((response) => {
                                         console.log('Risposta dal server:', response.data);
                                         localStorage.clear();
-                                        if (response.data.success == true) {                                            
-                                            // Esegui il redirect dopo 2 secondi
+
+                                        if (response.data.success == true) {
+                                            // Mostra il loader
+                                            self.isLoading = true;
+                                            // Nasconde il loader dopo 1.5 sec
                                             setTimeout(() => {
-                                                self.$router.push({ name: 'home' });
-                                                // Chiudi il modal dopo aver eseguito il redirect
-                                                // Imposta il pagamento su successo
-                                                self.paymentStatus = true;
-                                            }, 2000);
+                                                setTimeout(() => {
+                                                    self.isLoading = false;
+                                                }, 2500);
+                                                // Esegui il redirect dopo 2 secondi
+                                                self.$router.push({ name: 'home' })
+                                                    .then(() => {
+                                                        // Dopo il redirect, esegui il reload della pagina
+                                                        window.location.reload();
+                                                    });
+                                            }, 3000);
                                         } else {
                                             // Imposta il pagamento su errore
                                             self.paymentStatus = false;
@@ -185,9 +200,13 @@ export default {
     created() {
         // Leggi i dati del carrello dal localStorage all'avvio del componente
         const cartData = localStorage.getItem('cart');
+        const priceData = localStorage.getItem('price')
         if (cartData) {
             this.cart = JSON.parse(cartData);
             console.log(this.cart);
+        }
+        if (priceData){
+            this.currentTotal = JSON.parse(priceData);
         }
     },
     computed() {
@@ -202,8 +221,55 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import url(../../../style/partials/_variables.scss);
+.bg-img {
+    width: 100%;
+    height: 100%;
+    background-image: url(../../../public/wave-haikei-2.svg);
+    top: 0;
+    position: absolute;
+    z-index: -10;
+}
+
 .w-150 {
     margin-top: 150px;
+}
+
+/* RIEPILOGO ORDINE */
+
+.summary {
+    padding: 20px;
+    border: 1px solid gray;
+    border-radius: 10px;
+    background: rgba(134, 134, 134, 0.5);
+    -webkit-backdrop-filter: blur(10px);
+    backdrop-filter: blur(10px);
+    border: 1px solid rgba(134, 134, 134, 0.25);
+    display: flex;
+    flex-direction: column;
+}
+
+.summary_list{
+    display: flex;
+    margin: 0;
+    padding: 15px 0px;
+    justify-content: space-between;
+    font-weight: 600;
+}
+
+.summary_list_container{
+    flex-grow: 1;
+    overflow: hidden;
+}
+
+.customer-form{
+    padding: 20px;
+    border: 1px solid gray;
+    border-radius: 10px;
+    background: rgba(134, 134, 134, 0.5);
+    -webkit-backdrop-filter: blur(10px);
+    backdrop-filter: blur(10px);
+    border: 1px solid rgba(134, 134, 134, 0.25);
 }
 
 .show {
@@ -243,4 +309,100 @@ export default {
     background-color: #8bdda8;
     color: white;
 }
-</style>
+
+.button--gray {
+    outline: none;
+    background-color: #949695;
+    border-color: #949695;
+    color: white;
+    transition: all 200ms ease;
+}
+
+.button--gray:hover {
+    background-color: #838383;
+    color: white;
+}
+
+.btn-form{
+    background-color: #01ccbc;
+    border: none;
+    &:hover{
+        background-color: #05ddcb;
+    }
+}
+
+.border_btm{
+    border-bottom: 0.5px solid rgba(150, 150, 150, 0.5);
+}
+
+.border_top{
+    border-top: 0.5px solid rgba(150, 150, 150, 0.5);
+}
+
+/* LOADER */
+
+.modal-content {
+    background: rgba(61, 254, 214, 0.4);
+    backdrop-filter: blur(30px);
+    border: 1px solid rgba(61, 254, 214, 0.25);
+    position: relative;
+
+    .modal-header {
+        border: none;
+    }
+
+    .modal-footer {
+        border: none;
+    }
+}
+
+.loading {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 99999;
+}
+
+.loader {
+    transform: translateZ(1px);
+}
+
+.loader:after {
+    content: '$';
+    display: inline-block;
+    width: 70px;
+    height: 70px;
+    border-radius: 50%;
+    text-align: center;
+    line-height: 62px;
+    font-size: 32px;
+    font-weight: bold;
+    background: #FFD700;
+    color: #DAA520;
+    border: 4px double;
+    box-sizing: border-box;
+    box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, .1);
+    animation: coin-flip 4s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+}
+
+@keyframes coin-flip {
+
+    0%,
+    100% {
+        animation-timing-function: cubic-bezier(0.5, 0, 1, 0.5);
+    }
+
+    0% {
+        transform: rotateY(0deg);
+    }
+
+    50% {
+        transform: rotateY(1800deg);
+        animation-timing-function: cubic-bezier(0, 0.5, 0.5, 1);
+    }
+
+    100% {
+        transform: rotateY(3600deg);
+    }
+}</style>
--- Vue_project/src/components/pages/Homepage.vue
+++ Vue_project/src/components/pages/Homepage.vue
@@ -20,7 +20,7 @@ export default {
   data() {
     return {
       store,
-      // dataRT: [],
+      isLoading: true,
       BASE_URL: 'http://127.0.0.1:8000/api',
     };
   },
@@ -38,17 +38,95 @@ export default {
   },
   created() {
     this.fetchRestaurants();
+  },mounted() {
+    // Simula il caricamento della pagina con un ritardo di 2 secondi
+    setTimeout(() => {
+      // Dopo il ritardo, imposta isLoading su false per nascondere il loader
+      this.isLoading = false;
+    }, 2000);
   },
 };
 </script>
 <template>
-  <AppJumbo />
-  <JumboCarousel/>
-  <HomepageTypes/> 
-  <HomepageTracking/>
-  <HomepageSales/>
-  <HomepageCards/>
+  
+  <!-- Mostra il loader se isLoading è true -->
+  <div v-if="isLoading" class="loading">
+    <span class="loader"></span>
+    <p>Caricamento . . .</p>
+  </div>
+  <div v-else>
+    <AppJumbo />
+    <JumboCarousel/>
+    <HomepageTypes/> 
+    <HomepageTracking/>
+    <HomepageSales/>
+    <HomepageCards/>    
+  </div>
 
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.loading{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  top: 50%;
+  transform: scale(2);
+
+}
+.loader {
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  display: inline-block;
+  position: relative;
+  border: 3px solid;
+  border-color: #dddd #dddd transparent transparent;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+}
+.loader::after,
+.loader::before {
+  content: '';  
+  box-sizing: border-box;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  border: 3px solid;
+  border-color: transparent transparent #01ccbc #01ccbc;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  box-sizing: border-box;
+  animation: rotationBack 0.5s linear infinite;
+  transform-origin: center center;
+}
+.loader::before {
+  width: 32px;
+  height: 32px;
+  border-color: #dddd #dddd transparent transparent;
+  animation: rotation 1.5s linear infinite;
+}
+    
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+} 
+@keyframes rotationBack {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(-360deg);
+  }
+}
+    
+</style>
